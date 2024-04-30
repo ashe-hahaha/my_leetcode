cbf8b053fc4777b09272fb192f82be2af49c1caf
ZLY <953472069@qq.com>
2024-04-30 16:34:42 +0800

    35,2min

--- 35.搜索插入位置.py
+++ 35.搜索插入位置.py
@@ -7,5 +7,9 @@
 # @lc code=start
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if target<=nums[i]:
+                return i
+        return i+1
 # @lc code=end
 
