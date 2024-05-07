88f586717e9c0fbfe5e26fac19313cb7c9f7f6b5
ZLY <953472069@qq.com>
2024-05-07 13:38:46 +0800

    380. 60min

--- 380.o-1-时间插入、删除和获取随机元素.py
+++ 380.o-1-时间插入、删除和获取随机元素.py
@@ -8,24 +8,35 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.set1 = []
+        self.list = list()
+        self.dict = dict()
 
     def insert(self, val: int) -> bool:
-        if val in self.set1:
+        if val in self.list:
             return False
         else: 
-            self.set1.append(val)
+            # dict的key是唯一的
+            self.dict[val] = len(self.dict)
+            self.list.append(val)
             return True
 
     def remove(self, val: int) -> bool:
-        if val in self.set1:
-            del(self.set1[self.set1.index(val)])
+        if val in self.list:
+            # 找到val所在下标
+            index = self.dict[val]
+            # list最后一位移到下标处
+            self.list[index] = self.list[-1]
+            # 更新dict
+            self.dict[self.list[-1]] = index
+            # 移除最后一位
+            self.list.pop()
+            del self.dict[val]
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return choice(self.set1)
+        return choice(self.list)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
